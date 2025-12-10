36de7a1385dccaac65528e0fd79f4884f088ee0d
LEXY MANGKU SAPUTRA <147526693+LexyMangkuNEGORO25@users.noreply.github.com>
2025-12-10 21:39:51 +0700

    Update Detail.jsx

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link, useNavigate, useLocation } from "react-router-dom";
-import { API_BASE, IMAGE_BASE } from "../config/api";
 
+const API = `http://${window.location.hostname}:5000/api`;
 
 export default function Detail() {
   const { id } = useParams();
